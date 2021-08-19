98f712278663606b0e08c080dc0b123cc74c9579
Marcelo Garofalo <89047714+marcelombg@users.noreply.github.com>
2021-08-18 21:32:35 -0300

    Commit inicial
    Pasta completa CTD

---
+++ CTD/Programação Imperativa/aula05.js
@@ -0,0 +1,8 @@
+function converte(polegadas) {
+    return polegadas * 2.54;
+}
+console.log(converte(2))
+
+function receber(url) {
+    return polegadas * 2.54;
+}
---
+++ CTD/Programação Imperativa/calculadora.js
@@ -0,0 +1,23 @@
+function adicionar(a,b){
+    return console.log(a + b)
+}
+
+function subtração(a,b){
+    return console.log(a + b)
+}
+
+function multiplição(a,b){
+    return console.log(a + b)
+}
+
+function divisão(a,b){
+    return console.log(a + b)
+}
+
+adicionar(1,2)
+subtração(3,4)
+multiplição(5,6)
+divisão(7,8)
+
+//Grupo: Diego Reis, Filipe Farias, Marcelo Garofalo, Michel Picozzi,
+//Eduardo Russo, João B de Sousa Filho
