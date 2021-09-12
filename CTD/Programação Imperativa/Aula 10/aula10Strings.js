e8786130c88aed5e4cccde81f21abe5f9e183acd
Marcelo Garofalo <89047714+marcelombg@users.noreply.github.com>
2021-09-12 18:40:33 -0300

    PI

---
+++ CTD/Programação Imperativa/Aula 10/aula10Strings.js
@@ -0,0 +1,19 @@
+let texto = "Olá mundo, eu sou uma string"
+console.log (texto.length) //length 1
+
+console.log ('Olá mundo, eu sou uma string'.length) //length 2
+
+console.log (texto.indexOf("str")) //index of 1
+
+console.log ("Olá mundo".indexOf("uma")) //index of 2
+
+console.log ("Eu gosto de JS".slice(10,14))// slice
+
+console.log (" Sou um ser humano ".trim())// trim
+
+console.log ("Sou do planeta Terra".split(" "))// split
+
+console.log ("Sou programador PHP".replace("Sou", "Vou ser"))// replace
+
+
+
