7330d1b261d14d1c6eef0f831681983839b5163f
Marcelo Garofalo <89047714+marcelombg@users.noreply.github.com>
2022-08-16 10:58:33 -0300

    Aula 05

---
+++ CTD/Back End I/A5A - Proxy pattern/Mesa_Proxy_pattern/src/ServicoDownload.java
@@ -0,0 +1,8 @@
+public class ServicoDownload implements Donwload{
+
+
+    @Override
+    public void donwloadUsuario(int id, String nome, String email, String tipoUsuario) {
+        System.out.println("Download liberado");
+    }
+}
---
+++ CTD/Back End I/A5A - Proxy pattern/Proxy_Pattern/src/Vacinar.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public interface Vacinar {
+
+    public void vacinarPessoa(String rg, Date dataVacina, String tipoVacina);
+
+}
