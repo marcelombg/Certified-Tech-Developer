66ab60e80e4ade50e47b19d8c007d6f6d5fc0843
marcelo.muniz <marcelo_mbg@hotmail.com>
2022-10-19 22:27:55 -0300

    commit 1 aula 2

---
+++ Front End III/Marcelos's React Project/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,9 @@
+import './style.scss'
+
+export function SegundaAula () {
+    return (
+    <main>
+        <h1>Segunda Aula</h1>
+    </main>
+    )
+}
---
+++ Front End III/Marcelos's React Project/src/pages/Login/index.jsx
@@ -0,0 +1,5 @@
+import './style.scss'
+
+export function Login() {
+    return (    <h1>Eu sou o componente de Login</h1>)
+}
