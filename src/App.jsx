4f0a9c74d21c896c98780789c24f0c31b7cd120f
Ivin Rodrigues DH <ivin@digitalhouse.com>
2022-10-24 23:28:18 -0300

    Adição do projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { QuartaAula } from './lessons/QuartaAula'
+import { TerceiraAula } from './lessons/TerceiraAula'
+
+function App() {
+
+  return (
+    <QuartaAula />
+  )
+
+}
+
+export default App
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,4 @@
+export function Login() {
+    let title = 'Teste'
+    return (<h1 className="">{title}</h1>)
+}
