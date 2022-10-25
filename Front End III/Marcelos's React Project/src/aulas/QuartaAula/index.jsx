d074bac498de757bdc3e533cf2f3632190b42ba1
marcelo.muniz <marcelo_mbg@hotmail.com>
2022-10-24 22:48:30 -0300

    adicionando a key na lista

--- Front End III/Marcelos's React Project/src/aulas/QuartaAula/index.jsx
+++ Front End III/Marcelos's React Project/src/aulas/QuartaAula/index.jsx
@@ -5,16 +5,19 @@ export function QuartaAula () {
 
     const componentsFinded = [
         {
+            id: 1,
             title: 'Vídeo',
             image: youtubeimage,
             text: "Lorem ipsum dolor sit amet."
         },
         {
+            id: 2,
             title: 'Input',
             image: youtubeimage,
             text: "Lorem ipsum dolor sit amet."
         },
         {
+            id: 3,
             title: 'Vídeo 2',
             image: youtubeimage,
             text: "Lorem ipsum dolor sit amet."
@@ -25,33 +28,31 @@ export function QuartaAula () {
             text: "Lorem ipsum dolor sit amet."
         },
         {
+            id: 4,
             title: 'Vídeo 3',
             image: youtubeimage,
             text: "Lorem ipsum dolor sit amet."
         },
         {
+            id: 5,
             title: 'Input 3',
             image: youtubeimage,
             text: "Lorem ipsum dolor sit amet."
         }
     ]
 
-
-
     return (
         <div className='quarta-aula-component'>
             <h1 className='main-title'>Componentes identificados</h1>
-        
             <ul className='components-finded'>
                 {componentsFinded.map(item =>
-                <li>
+                <li key={item.id}>
                     <img src={item.image} alt="" />
                     <h1 >{item.title}</h1>
                     <p>{item.title}</p>
                 </li>
                 )}
             </ul>
-        
         </div>
     )
 }
