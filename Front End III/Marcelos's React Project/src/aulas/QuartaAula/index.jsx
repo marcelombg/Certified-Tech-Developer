2ca4d06a2913572caaeafd99c3e3b4321bd5a3f2
marcelo.muniz <marcelo_mbg@hotmail.com>
2022-10-24 22:44:41 -0300

    aula 4 map finalizado

--- Front End III/Marcelos's React Project/src/aulas/QuartaAula/index.jsx
+++ Front End III/Marcelos's React Project/src/aulas/QuartaAula/index.jsx
@@ -1,10 +1,57 @@
 import './style.scss'
+import youtubeimage from "./../../assets/images/youtube_home.png"
 
 export function QuartaAula () {
 
+    const componentsFinded = [
+        {
+            title: 'Vídeo',
+            image: youtubeimage,
+            text: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: 'Input',
+            image: youtubeimage,
+            text: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: 'Vídeo 2',
+            image: youtubeimage,
+            text: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: 'Input 2',
+            image: youtubeimage,
+            text: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: 'Vídeo 3',
+            image: youtubeimage,
+            text: "Lorem ipsum dolor sit amet."
+        },
+        {
+            title: 'Input 3',
+            image: youtubeimage,
+            text: "Lorem ipsum dolor sit amet."
+        }
+    ]
+
+
+
     return (
         <div className='quarta-aula-component'>
-            <h1 className='main-title'>Olá, eu sou a quarta aula</h1>
+            <h1 className='main-title'>Componentes identificados</h1>
+        
+            <ul className='components-finded'>
+                {componentsFinded.map(item =>
+                <li>
+                    <img src={item.image} alt="" />
+                    <h1 >{item.title}</h1>
+                    <p>{item.title}</p>
+                </li>
+                )}
+            </ul>
+        
         </div>
     )
 }
