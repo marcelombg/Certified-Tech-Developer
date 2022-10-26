3d62b12e690b4765f7f47f98e64d4f4d5de2b0b6
marcelo.muniz <marcelo_mbg@hotmail.com>
2022-10-26 12:39:13 -0300

    alterando dados provider

--- Infraestrutura II/A4A - Infraestrutura como código A disciplina/main.tf
+++ Infraestrutura II/A4A - Infraestrutura como código A disciplina/main.tf
@@ -1,16 +1,19 @@
 provider "aws" {
     region = "us-east-2"
-    access_key = "AKIAV4YTWNKPJFGO72OG"
-    secret_key = "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"
+    access_key = "AKIA2DOXMKGDINKQJDKL"
+    secret_key = "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"
 }
 
-resource "aws_instance" "servidor-marcelo-1" {
+resource "aws_instance" "servidor_teste_1" {
     ami = "ami-097a2df4ac947655f"
     instance_type = "t2.micro"
-    key_name = "key-marcelo-2"  
+    key_name = "aws-key-marcelo-1"
+    tags = {
+      Name = "servidor_teste_1"
+    }
 }
 
 resource "aws_key_pair" "terraform-marcelo-aws" {
-    key_name = "key-marcelo-2"  
+    key_name = "aws-key-marcelo-1"  
     public_key = file("terraform-marcelo-aws.pub")
 }
