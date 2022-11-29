59e7382599ce0e3bc1cfc6aab9032ecc4a9e1ace
Ivin Rodrigues DH <ivin@digitalhouse.com>
2022-11-28 21:08:05 -0300

    Adição de Contextos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 import { InstagramTimeLine } from './challenges/InstagramTimeLine'
 import { QuartaAula } from './lessons/QuartaAula'
 import { QuintaAula } from './lessons/QuintaAula'
+import { SegundaAula } from "./lessons/SegundaAula"
 import { TerceiraAula } from './lessons/TerceiraAula'
 import { DHGames } from './pages/DHGames'
 import { SetimaAula } from './lessons/SetimaAula'
@@ -15,6 +16,9 @@ import { DecimaQuintaAula } from './lessons/DecimaQuintaAula'
 import { MainLayout } from "./components/MainLayout"
 import { ToDo } from "./pages/ToDo"
 
+import { ThemeProvider } from "./hooks/useTheme"
+import { Configurations } from "./pages/Configurations"
+
 function App() {
 
   const appRouter = createBrowserRouter([
@@ -23,23 +27,53 @@ function App() {
       element: <MainLayout />,
       children: [
         {
+          path: 'segunda-aula',
+          element: <SegundaAula />
+        },
+        {
+          path: 'terceira-aula',
+          element: <TerceiraAula />
+        },
+        {
+          path: 'quarta-aula',
+          element: <QuartaAula />
+        },
+        {
+          path: 'quinta-aula',
+          element: <QuintaAula />
+        },
+        {
+          path: 'setima-aula',
+          element: <SetimaAula />
+        },
+        {
+          path: 'decima-terceira-aula',
+          element: <DecimaTerceiraAula />
+        },
+        {
           path: 'decima-quarta-aula',
           element: <DecimaQuartaAula />
         },
         {
           path: 'decima-quinta-aula',
           element: <DecimaQuintaAula />
+        },
+        {
+          path: 'configurations',
+          element: <Configurations />
+        },
+        {
+          path: 'to-do',
+          element: <ToDo />
         }
       ]
-    },
-    {
-      path: 'to-do',
-      element: <ToDo />
     }
   ])
 
   return (
-    <RouterProvider router={appRouter} />
+    <ThemeProvider>
+      <RouterProvider router={appRouter} />
+    </ThemeProvider>
   )
 
 }
--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -1,61 +1,77 @@
+import { useEffect } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import { useTheme } from '../../hooks/useTheme'
 
 import './style.scss'
 
 export function MainLayout() {
 
-    const items = [{ id: 1, name: 'Reportagem 1' }, { id: 2, name: 'Reportagem 2' }]
+    // Importação do Tema utilização o Hook customizado "useTheme"
+    const { theme } = useTheme()
 
     return (
-        <div className="main-layout-component">
-
+        // Alteração da Classe baseado no valor do Tema
+        <div className={`main-layout-component ${theme}`}>
             <header className='main-layout-component-header'>
-
-                <h1>Layout Principal</h1>
-
-                <ul>
-
-                    <li>
-
-                        <Link to="decima-quarta-aula">Decima Quarta Aula</Link>
-
-                    </li>
-
-                    <li>
-
-                        <Link to="decima-quinta-aula">Decima Quinta Aula</Link>
-
-                    </li>
-
-                </ul>
-
-                {
-
-                    items.map(
-                        item => {
-                            return (
-                                <>
-                                    <Link to={`decima-quinta-aula/${item.id}`}>{item.name}</Link>
-                                    <br />
-                                </>
-                            )
-
-                        }
-
-                    )
-
-                }
-
+                <h1>Front-end Terceiro Bimestre - React</h1>
             </header>
-
+            <aside className='main-layout-component-aside'>
+                {/* <section>
+                    <h1>Componentes</h1>
+                    <ul>
+                        <li>
+                            <Link to="decima-quarta-aula">Decima Quarta Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="decima-quinta-aula">Decima Quinta Aula</Link>
+                        </li>
+                    </ul>
+                </section> */}
+                <section>
+                    <h1>Aulas</h1>
+                    <ul>
+                        <li>
+                            <Link to="segunda-aula">Segunda Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="terceira-aula">Terceira Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="quarta-aula">Quarta Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="quinta-aula">Quinta Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="setima-aula">Setima Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="decima-terceira-aula">Decima Terceira Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="decima-quarta-aula">Decima Quarta Aula</Link>
+                        </li>
+                        <li>
+                            <Link to="decima-quinta-aula">Decima Quinta Aula</Link>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h1>Telas</h1>
+                    <ul>
+                        <li>
+                            <Link to="to-do">To Do</Link>
+                        </li>
+                        <li>
+                            <Link to="configurations">Configurações</Link>
+                        </li>
+                    </ul>
+                </section>
+            </aside>
             <main className='main-layout-component-main'>
-
                 <p>O conteúdo será carregado aqui dentro:</p>
-
                 <Outlet />
-
             </main>
-
         </div>
     )
 
