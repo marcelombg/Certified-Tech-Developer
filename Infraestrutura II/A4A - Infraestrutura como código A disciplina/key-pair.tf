739964f0914fa91d5da568dff4154ef90b269b00
Marcelo Garofalo <89047714+marcelombg@users.noreply.github.com>
2022-12-05 19:33:24 -0300

    Removendo pasta antiga

--- Infraestrutura II/A4A - Infraestrutura como código A disciplina/key-pair.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_key_pair" "ec2_instances" {
-    key_name = "terraform-${var.usuario}-aws"  
-    public_key = file("terraform-marcelo-aws.pub")
-    tags = {
-        Name = "${var.usuario}-ec2-${count.index}"
-    }
-}
--- Infraestrutura II/A4A - Infraestrutura como código A disciplina/output.tf
+++
@@ -1,7 +0,0 @@
-output "ec2_instances_public" {
-  value = aws_instance.ec2_instances.*.public_ip
-}
-
-output "ec2_instances_private" {
-  value = aws_instance.ec2_instances.*.private_ip
-}
